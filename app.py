0002a19290f8ab1ef0ace5dc5640f8c2d1a8d0e9
Hassan <hassanssena@gmail.com>
2020-10-07 20:29:32 -0300

    app

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_restful import Api
 from services.cliente import Cliente, Clientes
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'DontTellAnyone'
 api = Api(app)
+CORS(app)
 
 @app.before_first_request
 def cria_banco():
